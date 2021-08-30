8e7ea23461a41504fbd69926e17cc2f5a9c09702
bethcha <89695634+bethcha@users.noreply.github.com>
2021-08-30 11:50:53 +0100

    added SimpleSearch

--- newsoverview/__init__.py
+++ newsoverview/__init__.py
@@ -4,4 +4,4 @@ from .protests import Protests
 from .travel import TravelWarnings
 from .terror_monitor import TerrorNews
 from .tr_world_news import TranslatedWorldNews
-
+from .simple_search import SimpleSearch

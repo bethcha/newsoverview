fe21cc2ffb1cafb1464b9d61aa2920ef34fd31ad
bethcha <89695634+bethcha@users.noreply.github.com>
2021-08-30 09:58:56 +0100

    updated module

--- newsoverview/__init__.py
+++ newsoverview/__init__.py
@@ -1,6 +1,7 @@
-from covid_news import CoronavirusNews  # covid.py
-from en_world_news import EnglishWorldNews  # english_speakers.py
-from protests import Protests  # taken from english, spanish, french, portuguese, german and russian
-from travel import TravelWarnings  # taken from the same as protests.py
-from terror_monitor import TerrorNews  # terror.py
-from tr_world_news import TranslatedWorldNews
+from .covid_news import CoronavirusNews
+from .en_world_news import EnglishWorldNews
+from .protests import Protests
+from .travel import TravelWarnings
+from .terror_monitor import TerrorNews
+from .tr_world_news import TranslatedWorldNews
+

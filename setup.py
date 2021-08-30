14cb7564b047b78ed785a13f52ed055ebd0cc437
bethcha <89695634+bethcha@users.noreply.github.com>
2021-08-30 20:18:22 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='newsoverview',
-    version='0.1',
+    version='0.0.2',
     packages=['newsoverview'],
     url='https://github.com/bethcha/newsoverview',
     license='MIT',

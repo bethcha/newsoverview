2eb9713b4fcc7088112df70eadf72fc5c734e51c
bethcha <89695634+bethcha@users.noreply.github.com>
2021-08-30 11:52:12 +0100

    added long description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(
     name='newsoverview',
     version='0.1',
@@ -9,5 +12,6 @@ setup(
     author='Bethan',
     author_email='89695634+bethcha@users.noreply.github.com',
     description='News at a glance',
+    long_description=long_description,
     install_requires=['pygooglenews', 'deep-translator']
 )

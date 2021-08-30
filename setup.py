37420ae51cc8389a30954da7315cf0107ab147c5
bethcha <89695634+bethcha@users.noreply.github.com>
2021-08-30 15:22:05 +0100

    made a readable readme.md

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='newsoverview',
@@ -13,5 +13,6 @@ setup(
     author_email='89695634+bethcha@users.noreply.github.com',
     description='News at a glance',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['pygooglenews', 'deep-translator']
 )
